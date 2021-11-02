74336d974d7353d49aad1cd011a0ebfcc02fe8f1
DauMoe <lehuuyhoang1111999@gmail.com>
2021-11-02 11:31:14 +0700

    Use SASS instead CSS

---
+++ src/components/BlackLIst/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import SideBar from "../SideBar";
+
+function BlackList (props) {
+    return(
+        <div className="container">
+            <SideBar/>
+        </div>
+    );
+}
+
+export default BlackList;
---
+++ src/components/SideBar/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./SideBar.sass";
+
+//Sass doc: https://sass-lang.com/guide
+
+function SideBar (props) {
+    const getWindowDimensions = () => {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+          width,
+          height
+        };
+      }
+
+    const [windowsSize, setWindowSize] = useState(getWindowDimensions());
+
+    useEffect(function() {
+        function handleResize()  {
+            setWindowSize(getWindowDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return function() {
+            window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+
+    return(
+        <div className="sidebar" style={{"height": windowsSize.height + 'px'}}>
+            <div className="sidebar_avatar">
+                <img src=""/>
+            </div>
+        </div>
+    );
+}
+
+export default SideBar;
