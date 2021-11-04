4ce4ec0c32f28394f503040ebb0a763176cc1c83
DauMoe <lehuuyhoang1111999@gmail.com>
2021-11-04 17:47:14 +0700

    Add 404 page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,30 @@
 import React from "react";
+import './NotFound.sass';
+import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {SELECT_ITEM} from "../Redux/ReducersAndActions/SideBar/SideBarActionsDefinition";
 
-const NotFound = () => {
+const NotFound = (props) => {
+    const dispatch      = useDispatch();
+    const ChangeState   = () => {
+        dispatch({
+            type: SELECT_ITEM,
+            index: 0
+        });
+    }
+
     return(
-        <div>Hmm, seem you're lost!</div>
+        <div className="notfound_wrap">
+            <div className="center-div text-center">
+                <span className="notfound_title">404</span>
+                <span className="notfound_desc">
+                    Hmm, seem you're lost! Let's go&nbsp;
+                    <Link to="/blacklist" onClick={ChangeState}>
+                        back
+                    </Link>
+                </span>
+            </div>
+        </div>
     );
 }
 
