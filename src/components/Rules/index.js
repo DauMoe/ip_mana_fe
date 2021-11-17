5868d43a6b1ab0b68cf5d4f83a03acfe896316ef
DauMoe <lehuyhoang1111999@gmail.com>
2021-11-17 08:38:55 +0700

    DOING Rules

--- src/components/Rules/index.js
+++ src/components/Rules/index.js
@@ -1,18 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import './../../GlobalStyle.sass';
-import './../../Animation.sass';
 import {LIST_RULES, RULE_INFO, WEB_BASE_NAME} from "../API_URL";
 import {useDispatch, useSelector} from "react-redux";
 import {ERROR, LOADED} from "../Redux/ReducersAndActions/Status/StatusActionsDefinition";
+import {Link} from "react-router-dom";
 
 
 function Rules (props) {
-    const {_title} = props;
-    const dispatch = useDispatch();
-    const {loading, error, _msg} = useSelector(state => state.Status);
-    const [detailData, setDetailData] = useState({});
-    const [rulesData, setRulesData] = useState([]);
-    const [searchKeyWord, setSearchKeyWord] = useState("");
+    const {_title}                              = props;
+    const dispatch                              = useDispatch();
+    const {loading, error, _msg}                = useSelector(state => state.Status);
+    const [detailData, setDetailData]           = useState({});
+    const [rulesData, setRulesData]             = useState([]);
+    const [searchKeyWord, setSearchKeyWord]     = useState("");
+    const [testRegex, setTestRegex]             = useState({notMatchRegex: false, value: ""});
     let OriginRulesData = [];
 
     const __FetchFunction = (URL, body, callback) => {
@@ -65,8 +66,22 @@ function Rules (props) {
     }
 
     const GetRuleInfo = item => {
+        setTestRegex({
+            ...testRegex,
+            notMatchRegex: false,
+            value: ""
+        })
         __FetchFunction(RULE_INFO, {id: item.id}, function(res) {
             setDetailData(res[0]);
+        });
+    }
+
+    const CheckRegex = e => {
+        let regex = new RegExp(detailData.regex);
+        setTestRegex({
+            ...testRegex,
+            value: e.target.value,
+            notMatchRegex: e.target.value === "" ? false : !regex.test(e.target.value)
         });
     }
 
@@ -75,6 +90,7 @@ function Rules (props) {
         __FetchFunction(LIST_RULES, undefined, function (res) {
             setRulesData(res);
             OriginRulesData = JSON.parse(JSON.stringify(res));
+            GetRuleInfo({id: res[0].id});
         });
     }, []);
 
@@ -91,7 +107,10 @@ function Rules (props) {
                             </div>
                         ) : rulesData.map((item, index) => {
                             return (
-                                <div className="list-item" key={item.id} onClick={_ => GetRuleInfo(item)}>
+                                <div
+                                    key={item.id}
+                                    className="list-item"
+                                    onClick={_ => GetRuleInfo(item)}>
                                     {item.name}
                                 </div>
                             )
@@ -99,29 +118,63 @@ function Rules (props) {
                     </div>
                 </div>
 
-                <div style={{width: 'calc(100% - 350px)', height: 'calc(100% - 30px)', marginLeft: '50px', display: 'inline-block', overflow: 'auto'}}>
-                    <p>{JSON.stringify(detailData)}</p>
+                <div style={{
+                    width: 'calc(100% - 350px)',
+                    height: 'calc(100% - 30px)',
+                    marginLeft: '50px',
+                    display: 'inline-block',
+                    overflow: 'auto',
+                    padding: '10px'}}>
+
+                    <div>
+                        <label htmlFor="name">
+                            <span className="bold">Rule's name:</span>
+                        </label>
+                        <input className="form-control" id="name" value={detailData.name} onChange={e => {
+                            setDetailData({
+                                ...detailData,
+                                name: e.target.value
+                            })
+                        }}/>
+                    </div>
+
+                    <div className="margin-top-20">
+                        <label htmlFor="desc">
+                            <span className="bold">Rule's description:</span>
+                        </label>
+                        <input className="form-control" id="desc" value={detailData.desc} onChange={e => {
+                            setDetailData({
+                                ...detailData,
+                                desc: e.target.value
+                            })
+                        }}/>
+                    </div>
+
+                    <div className="margin-top-20">
+                        <label htmlFor="reg">
+                            <span className="bold">Rule's regex: </span>
+                            (Regex instruction <Link target="_blank" to={{pathname: "https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference"}}>here</Link>)
+                        </label>
+                        <input className="form-control" id="reg" value={detailData.regex} onChange={e => {
+                            setDetailData({
+                                ...detailData,
+                                regex: e.target.value
+                            })
+                        }}/>
+                    </div>
+
+                    <div className="margin-top-20">
+                        <label htmlFor="test_regex">
+                            <span className="bold">Test regex: </span>
+                        </label>
+                        <input className="form-control" id="test_regex" placeholder="Type something" value={testRegex.value} onChange={CheckRegex}/>
+                        {testRegex.notMatchRegex && <small>Not match regex</small>}
+                    </div>
+
+                    <div className="margin-top-20">
+                        <small className="italic">(Created: {detailData.createdAt}, Last update: {detailData.updatedAt})</small>
+                    </div>
                 </div>
-
-                {/*<div className="col-3 padding-15" style={{*/}
-                {/*    height: 'calc(100% - 30px)'*/}
-                {/*}}>*/}
-                {/*    <input className="form-control" disabled={true} onKeyDown={SearchByRuleName} placeholder="Find by rule's name ..."/>*/}
-                {/*    <div className="list-container margin-top-10">*/}
-                {/*        {rulesData.length === 0 ? (*/}
-                {/*            <div>*/}
-                {/*                <span>No rule founded!</span>*/}
-                {/*            </div>*/}
-                {/*        ) : rulesData.map((item, index) => {*/}
-                {/*            return (*/}
-                {/*                <div className="list-item" key={item.id} onClick={_ => GetRuleInfo(item)}>*/}
-                {/*                    {item.name}*/}
-                {/*                </div>*/}
-                {/*            )*/}
-                {/*        })}*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
             </div>
         </div>
     );
